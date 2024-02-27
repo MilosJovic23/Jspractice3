f5d421bb95c9b47cca18755b284891cb338d2c52
MilosJovic23 <milos.mj992@gmail.com>
2024-02-27 20:00:31 +0100

    * removed spaces

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 //
 
-//
 
 var cenaProizvoda = Number(
 	document.getElementById("cena").getAttribute("data-cena")
 );
-
-// console.log(cenaProizvoda);
-
 var porez = cenaProizvoda * 0.2;
-
-// console.log(cenaProizvoda + porez);
 
 function cenaSaPdvom() {
 	alert("cena proizvoda sa porezom je " + (cenaProizvoda + porez));
 }
-
 document.getElementById("buyButton").addEventListener("click", cenaSaPdvom);
